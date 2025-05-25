ee98d3a47f546d6b004ad521b4a2260a34c27b3a
xcaeser <oxidase-airbase-0m@icloud.com>
2025-05-25 23:57:36 +0200

    refactor: simplify build.zig by removing unnecessary comments and static library code

--- build.zig
+++ build.zig
@@ -1,32 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // Standard target options and optimization
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
 
-    // Create the module
+    // Optional: expose module if reused
     _ = b.addModule("zli", .{
         .root_source_file = b.path("src/zli.zig"),
     });
 
-    // Add the library for consumers
-    const lib = b.addStaticLibrary(.{
-        .name = "zli",
-        .root_source_file = b.path("src/zli.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.linkLibC();
-    b.installArtifact(lib);
-
-    // Create tests
+    // Test runner
     const lib_test = b.addTest(.{
         .root_source_file = b.path("src/zli.zig"),
         .target = target,
         .optimize = optimize,
     });
-    lib_test.linkLibC();
 
     const run_test = b.addRunArtifact(lib_test);
     run_test.has_side_effects = true;
